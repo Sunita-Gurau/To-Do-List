11ec5cb4405a44b6776bffb689e309cfe8a29f5c
Sunita Gurau <sunita.gurau@introcept.co>
2022-06-03 15:46:37 +0545

    frontend design completed

---
+++ js/todolist.js
@@ -0,0 +1,9 @@
+const inputForm = document.getElementById('inputForm');
+const createButton = document.getElementById('createButton');
+
+
+
+createButton.addEventListener('click',()=>{
+   alert("clicked");
+   
+})
