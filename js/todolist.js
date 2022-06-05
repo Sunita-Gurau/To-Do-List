985e7fe00aba1155dc928eeacf6eca28c65bb6d4
Sunita Gurau <sunita.gurau@introcept.co>
2022-06-05 21:46:40 +0545

    added event listener in image

--- js/todolist.js
+++ js/todolist.js
@@ -1,9 +1,54 @@
-const inputForm = document.getElementById('inputForm');
+const todoInput = document.getElementById('todo-input');
 const createButton = document.getElementById('createButton');
+const hiddenTask = document.getElementById('hiddenTask');
+const blankButton = document.getElementById('blankButton');
+const tickButton = document.getElementById('tickButton');
+const todos = document.getElementById('todos');
 
+
+
+const toDoList = [];
 
 
 createButton.addEventListener('click',()=>{
-   alert("clicked");
-   
+
+    if (todoInput.value === '') {
+        alert("You must write something!");
+      }
+       else {
+           
+        const div=document.createElement("div")
+        div.classList.add('flex', 'px-[30px]', 'pb-7');
+
+        const tickBox=document.createElement("img");
+        tickBox.setAttribute("src", "images/Tick.png");
+        tickBox.setAttribute("class", "tick");
+
+
+        const para = document.createElement("p");
+        para.innerHTML = todoInput.value;
+        para.classList.add('text-slate-700', 'pl-3', 'text-lg', 'font-normal', 'leading-[23px]' , 'para');
+
+        div.append(tickBox);
+        div.append(para);
+        todos.append(div);
+        todoInput.value = "";
+      }
+
+    
+
+
+})
+
+
+todos.addEventListener('click',(e)=>{
+e.target.classList.toggle("tick")
+if(e.target.classList.value==="tick"){
+    e.target.src="./images/tick.png";
+}
+else{
+    e.target.src="./images/Untick.png";
+}
+e.target.nextSibling.classList.toggle("line-through");
+
 })
